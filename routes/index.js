7154fbd41d8ef09b4ec04b429c32e1ac61a6449f
luddekn <70239345+luddekn@users.noreply.github.com>
2025-10-24 12:27:23 +0200

    First commit

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,40 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
 router.get("/", async (req, res, next) => {
-  const url = new URL("https://steamcommunity.com/market/priceoverview/");
-  const params = {
-    appid: 730,
-    currency: 9,
-    market_hash_name: "Recoil Case",
-  };
+  try {
+    const url = new URL("https://steamcommunity.com/market/priceoverview/");
+    const params = {
+      appid: 730,
+      currency: 9,
+      market_hash_name: "Recoil Case",
+    };
 
-  Object.entries(params).forEach(([key, value]) =>
-    url.searchParams.set(key, value),
-  );
+    Object.entries(params).forEach(([key, value]) =>
+      url.searchParams.set(key, value),
+    );
 
-  const itemName = "Recoil Case";
-  let currentPrice = 0.0;
-  const apires = await fetch(url)
-    .then((api) => api.json())
-    .then((apires) => {
-      const rawprice = apires.lowest_price.split(" ")[0];
-      const price = parseFloat(rawprice.replace(",", "."));
-      currentPrice = price;
+    const itemName = "Recoil Case";
+    let currentPrice = 0.0;
+    const apires = await fetch(url)
+      .then((api) => api.json())
+      .then((apires) => {
+        const rawprice = apires.lowest_price.split(" ")[0];
+        const price = parseFloat(rawprice.replace(",", "."));
+        currentPrice = price;
+      });
+
+    res.render("index", {
+      title: "Express",
+      item: itemName,
+      price: currentPrice,
     });
+  } catch (err) {}
+});
 
-  res.render("index", {
-    title: "Express",
-    item: itemName,
-    price: currentPrice,
-  });
+router.post("/", async (req, res, next) => {
+  try {
+  } catch (err) {}
 });
 
 module.exports = router;

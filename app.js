20c85d6da86a823e4319be091c9b06defc34aeea
luddekn <70239345+luddekn@users.noreply.github.com>
2025-10-24 20:15:12 +0200

    Added dotenv and sequelize to app.js

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
 var indexRouter = require("./routes/index");
 
 var app = express();
+const db = require("./models/index");
+db.sequelize.sync({ force: false });
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -17,6 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.static(path.resolve(__dirname, "./node_modules/bootstrap/dist/")),
+);
+app.use(
+  express.static(
+    path.resolve(__dirname, "./node_modules/bootstrap-icons/font/"),
+  ),
+);
 
 app.use("/", indexRouter);
 
